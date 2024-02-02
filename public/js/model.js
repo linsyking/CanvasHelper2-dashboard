3f19ed540d56d7c5854c921401951e6abdeb5326
Pachakutiq <null@gmail.com>
2024-02-02 18:23:41 -0300

    feat: editable server URL

--- public/js/model.js
+++ public/js/model.js
@@ -1,4 +1,31 @@
-const api_url = 'http://localhost:9283';
+// Getting server URL from local storage
+function getServerURL() {
+    // If no server URL in local storage, prompt the user to enter it
+    var serverURL = localStorage.getItem("serverURL");
+
+    var defaultURL = "http://localhost:9283";
+
+    if (!serverURL) {
+        serverURL = window.prompt("Please enter the server URL:", defaultURL);
+        // If url not start with http:// or https://, add it
+        if (!serverURL.startsWith("http://") && !serverURL.startsWith("https://")) {
+            serverURL = "http://" + serverURL;
+        }
+        // If url end with /, remove it
+        if (serverURL.endsWith("/")) {
+            serverURL = serverURL.slice(0, -1);
+        }
+        // If closed without entering anything, use the default URL
+        if (!serverURL) {
+            serverURL = defaultURL;
+        }
+        localStorage.setItem("serverURL", serverURL);
+    }
+
+    return serverURL;
+}
+
+const api_url = getServerURL();
 
 function loadcheck() {
     $('.checkbox').each(function () {
